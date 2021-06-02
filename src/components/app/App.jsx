79931d92a6224f77d6c0ad0ab92934834269ee11
Cierra-McDonald <cierralauren15@gmail.com>
2021-06-02 09:40:05 -0700

    initial set up

---
+++ src/components/app/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MainPage from './containters/MainPage';
+
+export default function App() {
+  return (
+    <MainPage/>
+  );
+}
---
+++ src/components/app/containters/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>Main Page Container!</h1>
+    </div>
+  );
+
+};
+
+export default MainPage;
